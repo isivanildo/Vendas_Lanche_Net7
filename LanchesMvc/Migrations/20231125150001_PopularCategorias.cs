32a1a05f4bd9a58f1524dbf8e4cfeecb0edf1c33
Ivanildo Ferreira <isivanildo@hotmail.com>
2023-11-25 12:05:49 -0300

    Populando a tabela Categorias

---
+++ LanchesMvc/Migrations/20231125150001_PopularCategorias.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Categorias (CategoriaNome, Descricao) VALUES ('Normal', 'Lanche feito com ingredientes normais')");
+            mb.Sql("INSERT INTO Categorias (CategoriaNome, Descricao) VALUES ('Natural', 'Lanche feito com ingredientes integrais e naturais')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Categorias");
+        }
+    }
+}
